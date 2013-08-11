bf08c9e19b8c8eb1f8a860f9d720fbfdb983daa1
Ryan M. Gallagher <ryanmichaelgallagher@gmail.com>
2013-08-11 00:18:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+var buffer = new Buffer(16)
+buffer.write(fs.readFileSync('index.html',encoding));
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-   response.send('Hello World 2!');
+   response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
