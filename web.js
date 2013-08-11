e1c60154aeb4bf548f528cbefee6468bf7a95ac5
Ryan M. Gallagher <ryanmichaelgallagher@gmail.com>
2013-08-11 00:36:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var fs = require('index.html');
-var app = express();
+var fs = require('fs');
 
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   var k = fs.readFileSync('./index.html')
-   response.send(k.toString);
+   var buffer = fs.readFileSync('index.html')
+   var content = buffer.toString();
+   response.send(content);
 });
 
 var port = process.env.PORT || 5000;
